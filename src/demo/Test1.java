bb2bb33387901c16c0a50eb2ccb5d59d6a3c775d
zhfworks <18326902719@163.com>
2019-12-04 14:03:22 +0800

    增加了时间格式

--- src/demo/Test1.java
+++ src/demo/Test1.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 
 public class Test1 {
     public static void main(String[] args) {
@@ -9,5 +10,6 @@ public class Test1 {
         System.out.println("用于测试");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(LocalDate.now());
+        System.out.println(sdf.format(new Date()));
     }
 }
