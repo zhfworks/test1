3d40ae26dab4dfba98fc3e311673eb15463dd40d
zhfworks <18326902719@163.com>
2019-12-04 13:59:26 +0800

    增加了sout

--- src/demo/Test1.java
+++ src/demo/Test1.java
@@ -1,7 +1,13 @@
 package demo;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 public class Test1 {
     public static void main(String[] args) {
+
         System.out.println("用于测试");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(LocalDate.now());
     }
 }
